0a169ea06ca767d60679f83991586e67fa727f75
Ann_Ar0 <ann.artyuh90@gmail.com>
2025-02-23 19:20:06 +0200

    finish task

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,22 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const removeLastContact = async () => {
+try {
+    const contacts = await readContacts();
+
+    if (contacts.length === 0) {
+      console.log('Список контактів порожній');
+      return;
+    }
+
+    const removedContact = contacts.pop();
+    await writeContacts(contacts);
+
+    console.log(`Видалено останній контакт:`, removedContact);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeLastContact();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (contacts) => {
+  try {
+    const data = JSON.stringify(contacts, null, 2);
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+    console.log('Контакти успішно збережено!');
+  } catch (error) {
+    console.error('Помилка запису у файл:', error.message);
+  }
+};
